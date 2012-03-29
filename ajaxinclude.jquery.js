06789aa84c07fcf5c552ef0bcb10f8460ca182e4
Steven Hauser <stevenhauser@gmail.com>
2012-03-29 11:03:00 -0400

    Cached some data outside of the each loop, moved a few sections into functions, did an early return if there is no URL, the threshold is not met, or there is no method, did a check to see if the URL is already in the filelist, plus a couple other changes.

--- ajaxinclude.jquery.js
+++ ajaxinclude.jquery.js
@@ -8,69 +8,100 @@
     * Before:	<a href="..." data-before="articles/latest/fragment">Latest Articles</a>
     * After:	<a href="..." data-after="articles/latest/fragment">Latest Articles</a>
     * Also, the data-threshold attr allows a min width for this to apply.
-    * After domready, you can use it like this: 
+    * After domready, you can use it like this:
          $("[data-append],[data-replace],[data-after],[data-before]").ajaxInclude();
 */
 (function( $ ){
-	$.fn.ajaxInclude = function( proxy ) {
-		
+
+	$.fn.ajaxInclude = function( options ) {
+
 		var filelist = [],
-			els = this;
-		
-		return this.each(function( k ) {
-			var el			= $( this ),
-				target		= el.data( "target" ),
-				targetEl	= target && $( target ) || el,
-				threshold	= screen.width > parseFloat( el.data( "threshold" ) || 0 ),
-				methods		= [ "append", "replace", "before", "after" ],
-				method,
-				url;
+			$els = this,
+			numEls = $els.length,
+			screenWidth = screen.width,
+			opts;
 
-			if ( threshold ) {
+		if (typeof options === "object") {
+			opts = $.extend({}, defaults, options);
+		} else {
+			opts = $.extend({}, defaults);
+			opts.proxy = options;
+		}
 
-				for( var ml = methods.length, i=0; i < ml; i++ ){
-					if( el.is( "[data-" + methods[ i ] + "]" ) ){
-						method	= methods[ i ];
-						url		= el.data( method );
-					}
-				}
+		return $els.each(function( k ) {
 
-				if( method === "replace" ){
-					method += "With";
-				}
+			var $el			= $( this ),
+				data 		= $el.data(),
+				target		= data.target,
+				$target		= target && $( target ) || $el,
+				threshold	= screenWidth > parseFloat( data.threshold || 0 ),
+				method 		= extractMethod(data),
+				jqMethod	= opts.methods[method],
+				url 		= data[method],
+				proxy 		= opts.proxy;
 
-				if( url && method ){
-					
-					el
-						.data( "method", method )
-						.data( "url", url )
-						.data( "proxy", proxy )
-						.bind( "ajaxInclude", function(e, data){
-							var content = $(data);
-							
-							if( $(this).data( "proxy" ) ){
-								content = content.filter( "page[url=\"" + $(this).data( "url" ) + "\"]" );
-							}
-							$( this )[ $(this).data( "method" ) ]( content );	
-						});
-					
+			if (!threshold || !method || !url) { return; }
+
+			$el
+				.data( "method", method )
+				.data( "url", url )
+				.data( "proxy", proxy )
+				.bind( "ajaxInclude", function(e, data){
+					var content = $(data);
 					if( proxy ){
-						
-						filelist.push( url );
-						
-						if( k === els.length - 1 ){
-							url = proxy + filelist.join();
-						}
+						content = content.filter( "page[url=\"" + url + "\"]" );
 					}
-					
-					if( !proxy || k === els.length-1 ){
-						$.get( url, function( data ) {	
-							els.trigger( "ajaxInclude", [data] );
-						});
-					}
-				}
+					$el[jqMethod]( content );
+				});
 
+			if ( !isUrlInFilelist(url) ) { filelist.push(url); }
+			if ( k === numEls-1 ) { initAjaxRequest(); }
+
+		}); // $els.each()
+
+
+		// Checks if the URL is already in the filelist array
+		// so we don't get duplicate data from the server if multiple
+		// sections of the page request the same data. Don't know
+		// if there's a use case for this, but it's a precaution.
+		function isUrlInFilelist(url) {
+			return $.inArray(url, filelist) !== -1;
+		}
+
+
+		// Returns the data-* method attribute that's on the element,
+		// based on the ones in opts.methods.
+		// @TODO: Should the method attribute be normalized? Should
+		// there be a default method of "after" for instance,
+		// and users override that with `data-method="replace"`,
+		// and they always provide a `data-includeUrl` instead?
+		function extractMethod(elData) {
+			for (var m in opts.methods) {
+				if (elData[m]) { return m; }
 			}
-		});
+			return false;
+		}
+
+		// Initializes the AJAX request, prepending the `proxy`
+		// if there is one.
+		function initAjaxRequest() {
+			var url = (opts.proxy || '') + filelist.join();
+			$.get( url, function( data ) {
+				$els.trigger( "ajaxInclude", [data] );
+			});
+		}
+
+	}; // $.fn.ajaxInclude
+
+
+	var defaults = $.fn.ajaxInclude.defaults = {
+		proxy: null,
+		methods: {
+			append: "append",
+			replace: "replaceWith",
+			before: "before",
+			after: "after"
+		}
 	};
+
 })( jQuery );
