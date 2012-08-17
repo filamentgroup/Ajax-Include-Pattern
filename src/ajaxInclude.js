2c7ba31387847550a435a86521ec6334deb66d25
Scott Jehl <scott@scottjehl.com>
2012-08-17 15:11:34 -0500

    fixed up some jshint warnings

--- src/ajaxInclude.js
+++ src/ajaxInclude.js
@@ -5,68 +5,68 @@
  * Copyright (c) 2012 @scottjehl, Filament Group, Inc.
  * Licensed under the MIT license.
  */
- (function( $, undefined ){
- 	$.fn.ajaxInclude = function( proxy ) {
+(function( $, undefined ){
+	$.fn.ajaxInclude = function( proxy ) {
 		
- 		var filelist = [],
- 			els = this;
+		var filelist = [],
+			els = this;
 		
- 		return this.each(function( k ) {
- 			var el			= $( this ),
- 				w			= window,
- 				qualify		= el.attr( "data-media" ),
- 				methods		= [ "append", "replace", "before", "after" ],
- 				method,
- 				url;
+		return this.each(function( k ) {
+			var el			= $( this ),
+				w			= window,
+				qualify		= el.attr( "data-media" ),
+				methods		= [ "append", "replace", "before", "after" ],
+				method,
+				url;
 
- 			if ( !qualify || ( w.matchMedia && w.matchMedia( qualify ).matches ) ) {
+			if ( !qualify || ( w.matchMedia && w.matchMedia( qualify ).matches ) ) {
 
- 				for( var ml = methods.length, i=0; i < ml; i++ ){
- 					if( el.is( "[data-" + methods[ i ] + "]" ) ){
- 						method	= methods[ i ];
- 						url		= el.attr( "data-" + method );
- 					}
- 				}
+				for( var ml = methods.length, i=0; i < ml; i++ ){
+					if( el.is( "[data-" + methods[ i ] + "]" ) ){
+						method	= methods[ i ];
+						url		= el.attr( "data-" + method );
+					}
+				}
 
- 				if( method === "replace" ){
- 					method += "With";
- 				}
+				if( method === "replace" ){
+					method += "With";
+				}
 
- 				if( url && method ){
+				if( url && method ){
 					
- 					el
- 						.data( "method", method )
- 						.data( "url", url )
- 						.bind( "ajaxInclude", function(e, data){
- 							var content = $(data);
+					el
+						.data( "method", method )
+						.data( "url", url )
+						.bind( "ajaxInclude", function(e, data){
+							var content = $(data);
 							
- 							if( $(this).data( "proxy" ) ){
- 								content = content.filter( "entry[url=\"" + $(this).data( "url" ) + "\"]" ).html();
- 							}
- 							$( this )[ $(this).data( "method" ) ]( content );	
- 						});
+							if( $(this).data( "proxy" ) ){
+								content = content.filter( "entry[url=\"" + $(this).data( "url" ) + "\"]" ).html();
+							}
+							$( this )[ $(this).data( "method" ) ]( content );	
+						});
 					
- 					if( proxy ){
+					if( proxy ){
 						
- 						el.data( "proxy", proxy );
+						el.data( "proxy", proxy );
 						
- 						if( $.inArray( url, filelist ) === -1 ){
- 							filelist.push( url );
- 						}
+						if( $.inArray( url, filelist ) === -1 ){
+							filelist.push( url );
+						}
 						
- 						if( k === els.length - 1 ){
- 							url = proxy + filelist.join();
- 						}
- 					}
+						if( k === els.length - 1 ){
+							url = proxy + filelist.join();
+						}
+					}
 					
- 					if( !proxy || k === els.length-1 ){
- 						$.get( url, function( data ) {	
- 							( proxy ? els : el ).trigger( "ajaxInclude", [data] );
- 						});
- 					}
- 				}
+					if( !proxy || k === els.length-1 ){
+						$.get( url, function( data ) {	
+							( proxy ? els : el ).trigger( "ajaxInclude", [data] );
+						});
+					}
+				}
 
- 			}
- 		});
- 	};
- }( jQuery ));
+			}
+		});
+	};
+}( jQuery ));
