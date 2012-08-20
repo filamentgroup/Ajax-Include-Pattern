d4aee32312e3c9821246d66a88d0e86aa5f11eab
Scott Jehl <scott@scottjehl.com>
2012-08-20 17:54:45 -0500

    refactored to support post-pageload ajax-includes via matchMedia event listeners. Fixes #6

--- dist/ajaxInclude.js
+++ dist/ajaxInclude.js
@@ -1,4 +1,4 @@
-/*! Ajax-Include - v0.1.0 - 2012-08-17
+/*! Ajax-Include - v0.1.0 - 2012-08-20
 * http://filamentgroup.com/lab/ajax_includes_modular_content/
 * Copyright (c) 2012 @scottjehl, Filament Group, Inc.; Licensed MIT */
 
@@ -7,71 +7,108 @@
 * Copyright (c) 2012 @scottjehl, Filament Group, Inc.; Licensed MIT */
 
 (function( $, undefined ){
-	$.fn.ajaxInclude = function( proxy ) {
+	$.fn.ajaxInclude = function( options ) {
+		var w = window,
+			urllist = [],
+			elQueue = $(),
+			boundAttr = "data-ajax-bound",
+			o = {
+				proxy: null
+			};
 		
-		var filelist = [],
-			els = this;
+		// Option extensions
+		// Sting check: deprected. Formerly, proxy was the single arg.
+		if( typeof options === "string" ){
+			o.proxy = options;
+		}
+		else {
+			o = $.extend( o, options );
+		}
 		
-		return this.each(function( k ) {
-			var el			= $( this ),
-				w			= window,
-				qualify		= el.attr( "data-media" ),
-				methods		= [ "append", "replace", "before", "after" ],
+		// if it's a proxy, que the element and its url, if not, request immediately
+		function queueOrRequest( el ){
+			var url = el.data( "url" );
+			if( o.proxy && $.inArray( url, urllist ) === -1 ){
+				urllist.push( url );
+				elQueue = elQueue.add( el );
+			}
+			else{
+				makeReq( el.data( "url" ), el );
+			}
+		}
+		
+		// request a url and trigger ajaxInclude on elements upon response
+		function makeReq( url, els ){
+			$.get( url, function( data ) {	
+				els.trigger( "ajaxInclude", [data] );
+			});
+		}
+		
+		// if there's a url queue
+		function runQueue(){
+			if( urllist.length ){
+				makeReq( o.proxy + urllist.join( "," ), elQueue );
+				elQueue = $();
+				urllist = [];
+			}
+		}
+		
+		// bind a listener to a currently-inapplicable media query for potential later changes
+		function bindForLater( el, media ){
+			var mm = w.matchMedia( media );
+			function cb(){
+				queueOrRequest( el );
+				runQueue();
+				mm.removeListener( cb );
+			}
+			if( mm.addListener ){
+				mm.addListener( cb );
+			}
+		}
+		
+		// loop through els, bind handlers
+		this.not( "[" + boundAttr + "]" ).each(function( k ) {
+			var el = $( this ),
+				media = el.attr( "data-media" ),
+				methods = [ "append", "replace", "before", "after" ],
 				method,
-				fixedMethod,
 				url;
-
-			if ( !qualify || ( w.matchMedia && w.matchMedia( qualify ).matches ) ) {
-
-				for( var ml = methods.length, i=0; i < ml; i++ ){
-					if( el.is( "[data-" + methods[ i ] + "]" ) ){
-						method	= fixedMethod = methods[ i ];
-						url		= el.attr( "data-" + method );
-					}
+			
+			for( var ml = methods.length, i=0; i < ml; i++ ){
+				if( el.is( "[data-" + methods[ i ] + "]" ) ){
+					method  = methods[ i ];
+					url		= el.attr( "data-" + method );
 				}
-
-				if( method === "replace" ){
-					fixedMethod += "With";
-				}
-
-				if( url && fixedMethod ){
-					
-					el
-						.data( "method", fixedMethod )
-						.data( "methodattr", "data-" + method )
-						.data( "url", url )
-						.bind( "ajaxInclude", function(e, data){
-							var content = $(data);
-							
-							if( $(this).data( "proxy" ) ){
-								content = content.filter( "entry[url=\"" + $(this).data( "url" ) + "\"]" ).html();
-							}
-							$( this )
-								[ $(this).data( "method" ) ]( content )
-								.removeAttr( $(this).data( "methodattr" ) );								
-						});
-					
-					if( proxy ){
-						
-						el.data( "proxy", proxy );
-						
-						if( $.inArray( url, filelist ) === -1 ){
-							filelist.push( url );
-						}
-						
-						if( k === els.length - 1 ){
-							url = proxy + filelist.join();
-						}
+			}
+			
+			if( method === "replace" ){
+				method += "With";
+			}
+			
+			el
+				.data( "method", method )
+				.data( "url", url )
+				.attr( boundAttr, true )
+				.bind( "ajaxInclude", function(e, data){
+					var content = $(data);
+					if( o.proxy ){
+						content = content.filter( "entry[url=\"" + $(this).data( "url" ) + "\"]" ).html();
 					}
-					
-					if( !proxy || k === els.length-1 ){
-						$.get( url, function( data ) {	
-							( proxy ? els : el ).trigger( "ajaxInclude", [data] );
-						});
-					}
-				}
-
+					el[ el.data( "method" ) ]( content );								
+				});
+			
+			if ( !media || ( w.matchMedia && w.matchMedia( media ).matches ) ) {
+				queueOrRequest( el );
+			}
+			else if( media && w.matchMedia ){
+				bindForLater( el, media );
 			}
 		});
+		
+		// empty the queue for proxied requests
+		runQueue();
+		
+		// return elems
+		return this;
 	};
 }( jQuery ));
--- src/ajaxInclude.js
+++ src/ajaxInclude.js
@@ -4,10 +4,10 @@
 
 (function( $, undefined ){
 	$.fn.ajaxInclude = function( options ) {
-		
-		var filelist = [],
-			els = this,
-			completeAttr = "data-ajax-requested",
+		var w = window,
+			urllist = [],
+			elQueue = $(),
+			boundAttr = "data-ajax-bound",
 			o = {
 				proxy: null
 			};
@@ -21,62 +21,90 @@
 			o = $.extend( o, options );
 		}
 		
-		return this.not( "[" + completeAttr + "]" ).each(function( k ) {
-			var el			= $( this ),
-				w			= window,
-				qualify		= el.attr( "data-media" ),
-				methods		= [ "append", "replace", "before", "after" ],
+		// if it's a proxy, que the element and its url, if not, request immediately
+		function queueOrRequest( el ){
+			var url = el.data( "url" );
+			if( o.proxy && $.inArray( url, urllist ) === -1 ){
+				urllist.push( url );
+				elQueue = elQueue.add( el );
+			}
+			else{
+				makeReq( el.data( "url" ), el );
+			}
+		}
+		
+		// request a url and trigger ajaxInclude on elements upon response
+		function makeReq( url, els ){
+			$.get( url, function( data ) {	
+				els.trigger( "ajaxInclude", [data] );
+			});
+		}
+		
+		// if there's a url queue
+		function runQueue(){
+			if( urllist.length ){
+				makeReq( o.proxy + urllist.join( "," ), elQueue );
+				elQueue = $();
+				urllist = [];
+			}
+		}
+		
+		// bind a listener to a currently-inapplicable media query for potential later changes
+		function bindForLater( el, media ){
+			var mm = w.matchMedia( media );
+			function cb(){
+				queueOrRequest( el );
+				runQueue();
+				mm.removeListener( cb );
+			}
+			if( mm.addListener ){
+				mm.addListener( cb );
+			}
+		}
+		
+		// loop through els, bind handlers
+		this.not( "[" + boundAttr + "]" ).each(function( k ) {
+			var el = $( this ),
+				media = el.attr( "data-media" ),
+				methods = [ "append", "replace", "before", "after" ],
 				method,
 				url;
-
-			if ( !qualify || ( w.matchMedia && w.matchMedia( qualify ).matches ) ) {
-
-				for( var ml = methods.length, i=0; i < ml; i++ ){
-					if( el.is( "[data-" + methods[ i ] + "]" ) ){
-						method  = methods[ i ];
-						url		= el.attr( "data-" + method );
-					}
-				}
-
-				if( method === "replace" ){
-					method += "With";
-				}
-
-				if( url && method ){
-					
-					el
-						.data( "method", method )
-						.data( "url", url )
-						.attr( completeAttr, true )
-						.bind( "ajaxInclude", function(e, data){
-							var content = $(data);
-							
-							if( $(this).data( "proxy" ) ){
-								content = content.filter( "entry[url=\"" + $(this).data( "url" ) + "\"]" ).html();
-							}
-							$( this )[ $(this).data( "method" ) ]( content );								
-						});
-					
-					if( o.proxy ){
-						
-						el.data( "proxy", o.proxy );
-						
-						if( $.inArray( url, filelist ) === -1 ){
-							filelist.push( url );
-						}
-						
-						if( k === els.length - 1 ){
-							url = o.proxy + filelist.join();
-						}
-					}
-					
-					if( !o.proxy || k === els.length-1 ){
-						$.get( url, function( data ) {	
-							( o.proxy ? els : el ).trigger( "ajaxInclude", [data] );
-						});
-					}
+			
+			for( var ml = methods.length, i=0; i < ml; i++ ){
+				if( el.is( "[data-" + methods[ i ] + "]" ) ){
+					method  = methods[ i ];
+					url		= el.attr( "data-" + method );
 				}
 			}
+			
+			if( method === "replace" ){
+				method += "With";
+			}
+			
+			el
+				.data( "method", method )
+				.data( "url", url )
+				.attr( boundAttr, true )
+				.bind( "ajaxInclude", function(e, data){
+					var content = $(data);
+					if( o.proxy ){
+						content = content.filter( "entry[url=\"" + $(this).data( "url" ) + "\"]" ).html();
+					}
+					el[ el.data( "method" ) ]( content );								
+				});
+			
+			if ( !media || ( w.matchMedia && w.matchMedia( media ).matches ) ) {
+				queueOrRequest( el );
+			}
+			else if( media && w.matchMedia ){
+				bindForLater( el, media );
+			}
 		});
+		
+		// empty the queue for proxied requests
+		runQueue();
+		
+		// return elems
+		return this;
 	};
 }( jQuery ));
