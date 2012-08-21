f7ddd21d7a9ae103c9615b855d9e6009884fd461
Scott Jehl <scott@scottjehl.com>
2012-08-21 12:25:52 -0500

    added a unit test suite with tests covering all of the public api. Fixes #9

--- test/ajaxInclude_test.js
+++ test/ajaxInclude_test.js
@@ -22,5 +22,109 @@
       raises(block, [expected], [message])
   */
 
+	$(function(){
+
+		asyncTest( "ajaxInclude event triggers on the target element and event callback contains markup", 2, function(){
+			$( "#ev" )
+				.live( "ajaxInclude", function( e, data ){
+					ok( true );
+					ok( $( data ).length )
+					start();
+				} )
+				.ajaxInclude();
+		} );
+		
+		asyncTest( "Data-before is including content before target element", 1, function(){
+			$( "#db" )
+				.live( "ajaxInclude", function(){
+					ok( $( "#db" ).prev().is( "ul" ) );
+					start();
+				} )
+				.ajaxInclude();
+		} );
+		
+		asyncTest( "Data-after is including content after target element", 1, function(){
+			$( "#da" )
+				.live( "ajaxInclude", function(){
+					ok( $( "#da" ).next().is( "ul" ) );
+					start();
+				} )
+				.ajaxInclude();
+		} );
+		
+		asyncTest( "Data-replace replaces element with included content", 3, function(){
+			var dr = $( "#dr" );
+			
+			ok( dr.length === 1 );
+			
+			dr
+				.live( "ajaxInclude", function(){
+					ok( $(this).parent().length === 0 );
+					ok( $( "#dr" ).length === 0 );
+					start();
+				} )
+				.ajaxInclude();
+		} );
+		
+		asyncTest( "Data-append is appending content to target element", 1, function(){
+			$( "#dap" )
+				.live( "ajaxInclude", function(){
+					ok( $( "#dap" ).children().length === 1 );
+					start();
+				} )
+				.ajaxInclude();
+		} );
+		
+		
+		
+		asyncTest( "Data-media with applicable mq appends content as expected", 1, function(){
+			$( "#dm-a" )
+				.live( "ajaxInclude", function(){
+					ok( $( "#dm-a" ).children().length === 1 );
+					start();
+				} )
+				.ajaxInclude();
+		} );
+		
+		asyncTest( "Data-media with inapplicable mq prevents content from appending (assumes an unexpected request will return within 5 seconds)", 1, function(){
+			var response = null;
+			$( "#dm-i" )
+				.live( "ajaxInclude", function(){
+					response = true;
+				} )
+				.ajaxInclude();
+			
+			setTimeout( function(){
+				ok( response === null );
+				start();
+			}, 5000 );
+		} );
+		
+		/* tests for whether proxy integration is working */
+		if( $( "#proxytests").length ){
+			
+			
+			asyncTest( "Proxy includes multiple ajax includes via 1 request and appends in proper locations", 3, function(){
+				$( "#proxy-b" )
+					.live( "ajaxInclude", function( e, data ){
+						console.log($( "#proxy-a" ))
+						ok( $( "#proxy-a" ).children().length === 1 );
+						ok( $( "#proxy-b" ).children().length === 1 );
+						ok( $(data).filter("entry").length === 2 );
+						start();
+
+					} );
+					
+				$( "#proxy-a, #proxy-b" ).ajaxInclude( { proxy: "functional/quickconcat.php?wrap&files=" } );
+			} );
+			
+			
+			
+			
+		}
+		
+	});
+
+
 
 }(jQuery));
